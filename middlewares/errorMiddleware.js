14ef0f23ba6de0ac57a06e98c85dbf35eb20be6e
Eduardomak <eduardomak92@gmail.com>
2023-03-17 16:34:22 -0300

    test

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,4 +1,4 @@
-const errorMid = (err, _req, res, _next) => {
+const errorMid = (err, _req, res) => {
   console.log(err.message);
   if (err.status) return res.status(err.status).json({ message: err.message });
   return res.status(500).json({ message: 'Internal Server Error' });
